bc3d3ca53cf93bfc5ea64f144f8abdeaa37ea66f
uncannyvalle <jvalle0926@gmail.com>
2025-03-02 23:26:28 -0500

    add button component and update global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono, Karla} from "next/font/google";
+import {Karla} from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 const karla = Karla({
     variable: "--font-karla",
